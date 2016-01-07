3009181aa9e269c8a29cba521b1faa281a81663b
Tu TV <tutv95@gmail.com>
2016-01-08 00:42:00 +0700

    Init plugin

---
+++ debugging-wp.php
@@ -0,0 +1,14 @@
+<?php
+/*
+Plugin Name: Debugging WordPress
+Plugin URI: tutran.me
+Description: Plugin debug for WordPress Developer.
+Author: Max
+Version: 1.0.0
+Author URI: tutran.me
+*/
+
+/**
+ * Require library
+ */
+require_once 'vendor/autoload.php';
