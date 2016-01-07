4462103e4029f99a1c9915c6b938b385bc132bb4
Tu TV <tutv95@gmail.com>
2016-01-08 01:10:07 +0700

    Updated__

--- debugging-wp.php
+++ debugging-wp.php
@@ -6,9 +6,13 @@ Description: Plugin debug for WordPress Developer.
 Author: Max
 Version: 1.0.0
 Author URI: tutran.me
+License: GPLv2 or later
 */
 
+define( 'DEBUGGING_WP_VERSION', '1.0.0' );
+define( 'DEBUGGING_WP_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
+
 /**
- * Require library
+ * Require libraries
  */
-require_once 'vendor/autoload.php';
+require_once DEBUGGING_WP_PLUGIN_DIR . 'vendor/autoload.php';
---
+++ readme.txt
@@ -0,0 +1,9 @@
+=== Debuggin WP ===
+Tags: develop, debug
+Requires at least: 1.0.0
+Tested up to: 1.0.0
+Stable tag: 1.0.0
+License: GPLv2 or later
+Contributors: tutv95
+
+Plugin debug for WordPress Developer.
